30c4820658c50acb8716f3142ee66f3becfef54d
Ines Bosch-Alfonso <00monserrat@gmail.com>
2020-12-18 18:02:05 -0500

    Added a loading spinner

--- popup.js
+++ popup.js
@@ -1,6 +1,8 @@
 var quote =  document.querySelector(".quote");
 var author = document.querySelector(".author")
+var loader = document.querySelector(".loader");
 fetch(`https://favqs.com/api/qotd`).then(response => response.json()).then(data => {
+    loader.style.display = "none";
     quote.textContent = data["quote"]["body"];
     author.textContent = data["quote"]["author"];
 });
